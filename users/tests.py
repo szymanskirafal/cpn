2b4c8d04272a67f1c30e214b2a349d42fc56e994
Rafal Szymanski <r.szymansky@gmail.com>
2020-10-14 23:37:19 +0200

    Add test createsuperuser

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,12 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+
+class CustomUserTests(TestCase):
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username='superadmin',
+            email='superadmin@email.com',
+            password='testpass123'
+        )
