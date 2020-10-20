3df796e14d6a956b0b6aa3a51caf059ef190b619
Rafal Szymanski <r.szymansky@gmail.com>
2020-10-20 22:30:19 +0200

    Change Allowed Hosts

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG", default=False)
 
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=['.herokuapp.com', 'localhost', '127.0.0.1', '0.0.0.0', ])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=['cpntest.herokuapp.com', 'localhost', '127.0.0.1', '0.0.0.0', ])
 
 
 # Application definition
