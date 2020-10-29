0d5e23711eb94e45eb85a0b19e6a6d7c00b08395
Rafal Szymanski <r.szymansky@gmail.com>
2020-10-29 23:25:32 +0100

    Add phones page

--- pages/urls.py
+++ pages/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
-from .views import home_view
+from .views import home_view, phones_view
 
+app_name = "pages"
 urlpatterns = [
     path(
         '',
         view = home_view,
         name = 'home',
+    ),
+    path(
+        'phones/',
+        view = phones_view,
+        name = 'phones',
     ),
 
 ]
--- pages/views.py
+++ pages/views.py
@@ -4,4 +4,10 @@ from django.views import generic
 class HomePageTemplateView(generic.TemplateView):
     template_name = 'home.html'
 
+
+class PhonesTemplateView(generic.TemplateView):
+    template_name = 'phones.html'
+
 home_view = HomePageTemplateView.as_view()
+
+phones_view = PhonesTemplateView.as_view()
