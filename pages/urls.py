e6734ac925d535ca4fcd212af3cd65e52a0a48c9
Rafal Szymanski <r.szymansky@gmail.com>
2020-11-03 21:42:29 +0100

    Add subpages

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path
-from .views import home_view, contact_view
+from .views import (
+    home_view,
+    contact_view,
+    ekoterm_view,
+    environment_view,
+    fuels_view,
+    history_view,
+    oils_view,
+    privacy_view,
+    )
 
 app_name = "pages"
 urlpatterns = [
@@ -13,5 +22,36 @@ urlpatterns = [
         view = contact_view,
         name = 'contact',
     ),
+    path(
+        'ekoterm/',
+        view = ekoterm_view,
+        name = 'ekoterm',
+    ),
+    path(
+        'environment/',
+        view = environment_view,
+        name = 'environment',
+    ),
+    path(
+        'fuels/',
+        view = fuels_view,
+        name = 'fuels',
+    ),
+    path(
+        'history/',
+        view = history_view,
+        name = 'history',
+    ),
+    path(
+        'oils/',
+        view = oils_view,
+        name = 'oils',
+    ),
+    path(
+        'privacy/',
+        view = privacy_view,
+        name = 'privacy',
+    ),
+
 
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,13 +1,51 @@
 from django.views import generic
 
 
-class HomePageTemplateView(generic.TemplateView):
-    template_name = 'home.html'
-
 
 class ContactTemplateView(generic.TemplateView):
     template_name = 'contact.html'
 
-home_view = HomePageTemplateView.as_view()
+
+class EkotermTemplateView(generic.TemplateView):
+    template_name = 'ekoterm.html'
+
+
+class EnvironmentTemplateView(generic.TemplateView):
+    template_name = 'environment.html'
+
+
+class FuelsTemplateView(generic.TemplateView):
+    template_name = 'fuels.html'
+
+
+class HistoryTemplateView(generic.TemplateView):
+    template_name = 'history.html'
+
+
+class HomePageTemplateView(generic.TemplateView):
+    template_name = 'home.html'
+
+
+class OilsTemplateView(generic.TemplateView):
+    template_name = 'oils.html'
+
+
+class PrivacyTemplateView(generic.TemplateView):
+    template_name = 'privacy.html'
+
 
 contact_view = ContactTemplateView.as_view()
+
+ekoterm_view = EkotermTemplateView.as_view()
+
+environment_view = EnvironmentTemplateView.as_view()
+
+fuels_view = FuelsTemplateView.as_view()
+
+history_view = HistoryTemplateView.as_view()
+
+home_view = HomePageTemplateView.as_view()
+
+oils_view = OilsTemplateView.as_view()
+
+privacy_view = PrivacyTemplateView.as_view()
